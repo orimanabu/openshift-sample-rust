8ace47f6f9c5940a98c12bef91bdde87866bdd08
Manabu Ori <manabu.ori@gmail.com>
2023-03-30 14:50:00 +0900

    Print local IPv4 address

--- src/main.rs
+++ src/main.rs
@@ -28,5 +28,43 @@ async fn handler(ConnectInfo(addr): ConnectInfo<SocketAddr>, req: Request<hyper:
     };
 
     let local_datetime: DateTime<Local> = Local::now();
-    format!("{} Hello, world: HOST={}, RemoteAddr={}\n", local_datetime, server, addr)
+    format!("{} Hello, world: HOST={}, LocalAddr={}, RemoteAddr={}\n", local_datetime, server, get_ip_list()[0], addr)
+}
+
+// https://qiita.com/mm_sys/items/e3a62ddb3f8dbeee9031
+#[cfg(target_os = "windows")]
+fn get_ip_list() -> Vec<String> {
+    use ipconfig;
+    let mut ips: Vec<String> = Vec::new();
+    match ipconfig::get_adapters() {
+        Ok(adapters) => {
+            for adapter in adapters {
+                if adapter.oper_status() == ipconfig::OperStatus::IfOperStatusUp {
+                    for address in adapter.ip_addresses() {
+                        if !address.is_loopback() && address.is_ipv4() {
+                            ips.push(address.to_string());
+                        }
+                    }
+                }
+            }
+        }
+        _ => {}
+    }
+    ips
+}
+
+#[cfg(not(target_os = "windows"))]
+fn get_ip_list() -> Vec<String> {
+    use pnet::datalink;
+    let mut ips: Vec<String> = Vec::new();
+    for interface in datalink::interfaces() {
+        if !interface.ips.is_empty() && interface.is_up() {
+            for ip_net in interface.ips {
+                if ip_net.is_ipv4() && !ip_net.ip().is_loopback() {
+                    ips.push(ip_net.ip().to_string());
+                }
+            }
+        }
+    };
+    ips
 }
