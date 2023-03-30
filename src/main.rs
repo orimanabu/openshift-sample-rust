70149779f14b6746b38e99e5d78d60263fd021c4
Manabu Ori <manabu.ori@gmail.com>
2023-03-30 14:35:09 +0900

    Remove unnecessary comments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use axum::{
     Router,
 };
 use axum_macros::debug_handler;
-// use hyper::{header::HOST, Request, StatusCode};
 use hyper::{header::HOST, Request};
 use std::net::SocketAddr;
 
@@ -20,30 +19,12 @@ async fn main() {
         .expect("server failed");
 }
 
-// impl Default for ConnectInfo {
-//     fn default() -> Self {
-//         Self { page: 1, per_page: 30 }
-//     }
-// }
-
-// async fn handler(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> String {
-//     format!("Hello {}", addr)
-// }
-
-// async fn handler(req: Request<hyper::Body>, ConnectInfo(addr): ConnectInfo<SocketAddr>) -> Result<String, StatusCode> {
-// async fn handler(req: Request<hyper::Body>, ConnectInfo(addr): ConnectInfo<SocketAddr>) -> String {
-// async fn handler(req: Request<hyper::Body>, conninfo: Option<ConnectInfo<SocketAddr>>) -> String {
 #[debug_handler]
-// async fn handler(req: Request<hyper::Body>, conninfo: Option<ConnectInfo<SocketAddr>>) -> impl IntoResponse {
 async fn handler(ConnectInfo(addr): ConnectInfo<SocketAddr>, req: Request<hyper::Body>) -> impl IntoResponse {
-    // let ConnectInfo(addr) = conninfo.unwrap_or_default();
-    // let ConnectInfo(addr) = conninfo.unwrap();
-
     let server = match req.headers().get(HOST) {
         Some(header) => header.to_str().unwrap(),
         None => "Unknown",
     };
 
     format!("Hello from {} to {}", server, addr)
-    // print!("Hello from {} to {}", server, addr);
 }
